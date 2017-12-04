57ebeb1c2d2c80fffaa018f8e689efc67bd811f2
Luis Portillo <lcpdiseno@gmail.com>
2017-12-03 20:53:25 -0600

    Armado de header principal

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,6 +2,10 @@ const header = () => {
 
 return  `
     <header class="mainHeader">
+        <div class="centerContainer">
+            <h2 class="mainHeader-phrase">¡Busca un experto, prúebalo y califícalo</h2>
+            <img class="mainHeader-image" src="./src/components/header/img/header_img.svg">
+        </div>
     </header>  
 ` 
 }
